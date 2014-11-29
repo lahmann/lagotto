dc0729a29253fd8784b9182bd3eb3d68b4bea73a
Martin Fenner <mfenner@plos.org>
2014-11-29 11:38:54 +0100

    updated ember router

--- ember-app/app/router.js
+++ ember-app/app/router.js
@@ -5,6 +5,9 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
+var inflector = Ember.Inflector.inflector;
+inflector.uncountable('status');
+
 Router.map(function() {
   this.resource('works', function() { });
   this.resource('sources', function() { });
@@ -13,6 +16,7 @@ Router.map(function() {
   this.resource('notifications', function() { });
   this.resource('publishers', function() { });
   this.resource('docs', function() { });
+  this.resource('status', function() { });
 });
 
 export default Router;
