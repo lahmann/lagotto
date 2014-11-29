f83229995d88f75342208ae9b878b5b2bae5434e
Martin Fenner <mfenner@plos.org>
2014-11-29 11:36:59 +0100

    added ember application adapter

---
+++ ember-app/app/adapters/application.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.ActiveModelAdapter.extend({
+  namespace: 'api/v6',
+  headers: {
+    "Authorization": "Token token=8897f9349100728d66d64d56bc21254bb346a9ed21954933"
+  }
+});
