9e5ffc57f51d5f3b712efae993f9e188431d91e2
Martin Fenner <mfenner@plos.org>
2015-01-02 22:09:05 +0100

    format researchblogging events as CSL. #213

--- spec/models/researchblogging_spec.rb
+++ spec/models/researchblogging_spec.rb
@@ -49,14 +49,12 @@ describe Researchblogging, type: :model, vcr: true do
     it "should report if the doi is missing" do
       work = FactoryGirl.build(:work, :doi => "")
       result = {}
-      result.extend Hashie::Extensions::DeepFetch
       expect(subject.parse_data(result, work)).to eq(events: [], :events_by_day=>[], :events_by_month=>[], events_url: nil, event_count: 0, event_metrics: { pdf: nil, html: nil, shares: nil, groups: nil, comments: nil, likes: nil, citations: 0, total: 0 })
     end
 
     it "should report if there are no events and event_count returned by the ResearchBlogging API" do
       body = File.read(fixture_path + 'researchblogging_nil.xml')
       result = Hash.from_xml(body)
-      result.extend Hashie::Extensions::DeepFetch
       response = subject.parse_data(result, work)
       expect(response).to eq(events: [], :events_by_day=>[], :events_by_month=>[], event_count: 0, event_metrics: { pdf: nil, html: nil, shares: nil, groups: nil, comments: nil, likes: nil, citations: 0, total: 0 }, events_url: subject.get_events_url(work))
     end
@@ -65,7 +63,6 @@ describe Researchblogging, type: :model, vcr: true do
       work = FactoryGirl.build(:work, :doi => "10.1371/journal.pone.0035869", published_on: "2009-07-01")
       body = File.read(fixture_path + 'researchblogging.xml')
       result = Hash.from_xml(body)
-      result.extend Hashie::Extensions::DeepFetch
       response = subject.parse_data(result, work)
       expect(response[:event_count]).to eq(8)
       expect(response[:events].length).to eq(8)
@@ -77,22 +74,18 @@ describe Researchblogging, type: :model, vcr: true do
       expect(response[:events_by_month].first).to eq(year: 2009, month: 7, total: 1)
 
       event = response[:events].first
-
-      expect(event[:event_csl]['author']).to eq([{"family"=>"Spoetnik", "given"=>"Laika"}])
-      expect(event[:event_csl]['title']).to eq("Why Publishing in the NEJM is not the Best Guarantee that Something is True: a Response to Katan")
-      expect(event[:event_csl]['container-title']).to eq("Laika's Medliblog")
-      expect(event[:event_csl]['issued']).to eq("date-parts"=>[[2012, 10, 27]])
-      expect(event[:event_csl]['type']).to eq("post")
-
-      expect(event[:event_time]).to eq("2012-10-27T11:32:09Z")
-      expect(event[:event_url]).to eq(event[:event]["post_URL"])
+      expect(event['URL']).to eq("http://laikaspoetnik.wordpress.com/2012/10/27/why-publishing-in-the-nejm-is-not-the-best-guarantee-that-something-is-true-a-response-to-katan/")
+      expect(event['author']).to eq([{"family"=>"Spoetnik", "given"=>"Laika"}])
+      expect(event['title']).to eq("Why Publishing in the NEJM is not the Best Guarantee that Something is True: a Response to Katan")
+      expect(event['container-title']).to eq("Laika's Medliblog")
+      expect(event['issued']).to eq("date-parts"=>[[2012, 10, 27]])
+      expect(event['type']).to eq("post")
     end
 
     it "should report if there is one event returned by the ResearchBlogging API" do
       work = FactoryGirl.build(:work, :doi => "10.1371/journal.pone.0035869", published_on: "2012-10-01")
       body = File.read(fixture_path + 'researchblogging_one.xml')
       result = Hash.from_xml(body)
-      result.extend Hashie::Extensions::DeepFetch
       response = subject.parse_data(result, work)
       expect(response[:event_count]).to eq(1)
       expect(response[:events].length).to eq(1)
@@ -104,15 +97,12 @@ describe Researchblogging, type: :model, vcr: true do
       expect(response[:events_by_month].first).to eq(year: 2012, month: 10, total: 1)
 
       event = response[:events].first
-
-      expect(event[:event_csl]['author']).to eq([{"family"=>"Spoetnik", "given"=>"Laika"}])
-      expect(event[:event_csl]['title']).to eq("Why Publishing in the NEJM is not the Best Guarantee that Something is True: a Response to Katan")
-      expect(event[:event_csl]['container-title']).to eq("Laika's Medliblog")
-      expect(event[:event_csl]['issued']).to eq("date-parts"=>[[2012, 10, 27]])
-      expect(event[:event_csl]['type']).to eq("post")
-
-      expect(event[:event_time]).to eq("2012-10-27T11:32:09Z")
-      expect(event[:event_url]).to eq(event[:event]["post_URL"])
+      expect(event['URL']).to eq("http://laikaspoetnik.wordpress.com/2012/10/27/why-publishing-in-the-nejm-is-not-the-best-guarantee-that-something-is-true-a-response-to-katan/")
+      expect(event['author']).to eq([{"family"=>"Spoetnik", "given"=>"Laika"}])
+      expect(event['title']).to eq("Why Publishing in the NEJM is not the Best Guarantee that Something is True: a Response to Katan")
+      expect(event['container-title']).to eq("Laika's Medliblog")
+      expect(event['issued']).to eq("date-parts"=>[[2012, 10, 27]])
+      expect(event['type']).to eq("post")
     end
 
     it "should catch timeout errors with the ResearchBlogging API" do
