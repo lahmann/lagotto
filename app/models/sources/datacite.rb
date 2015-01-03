a269057afa218d184127141b380ff6e65c4e6054
Martin Fenner <mfenner@plos.org>
2015-01-03 10:25:06 +0100

    format datacite events as CSL. #213

--- app/models/sources/datacite.rb
+++ app/models/sources/datacite.rb
@@ -1,9 +1,21 @@
-# encoding: UTF-8
-
 class Datacite < Source
   def get_events(result)
-    result["response"] ||= {}
-    Array(result["response"]["docs"]).map { |item| { event: item, event_url: "http://doi.org/#{item['doi']}" } }
+    Array(result.fetch("response", {}).fetch("docs", nil)).map do |item|
+      doi = item.fetch("doi", nil)
+      year = item.fetch("publicationYear", nil).to_i
+      title = String(item.fetch("title", []).first).chomp(".")
+      type = item.fetch("resourceTypeGeneral", nil)
+      type = DATACITE_TYPE_TRANSLATIONS[type] if type
+
+      { "author" => get_authors(item.fetch('creator', []), sep: ", ", reversed: true),
+        "title" => title,
+        "container-title" => nil,
+        "issued" => { "date-parts" => [[year]] },
+        "DOI" => doi,
+        "URL" => get_url_from_doi(doi),
+        "publisher" => item.fetch("publisher", nil),
+        "type" => type }
+    end
   end
 
   def config_fields
@@ -11,7 +23,7 @@ class Datacite < Source
   end
 
   def url
-    config.url || "http://search.datacite.org/api?q=relatedIdentifier:%{doi}&fl=relatedIdentifier,doi,creator,title,publisher,publicationYear&fq=is_active:true&fq=has_metadata:true&indent=true&rows=100&wt=json"
+    config.url || "http://search.datacite.org/api?q=relatedIdentifier:%{doi}&fl=doi,creator,title,publisher,publicationYear,resourceTypeGeneral,datacentre,datacentre_symbol,prefix,relatedIdentifier&fq=is_active:true&fq=has_metadata:true&rows=1000&wt=json"
   end
 
   def events_url
